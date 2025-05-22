defc8bd043c4e498ebfff2f334cc0e3178361e94
callee <yth001209@gmail.com>
2025-05-22 06:53:54 +0000

    Doing MPI_SEND & local_board initialize

--- project3.cpp
+++ project3.cpp
@@ -4,10 +4,28 @@
 #include <iostream>
 #include <vector>
 #include <limits>
+#include <cmath>
 
 using namespace std;
 
-void readBoard(int m, int g, vector<vector<bool>>& board){
+void initDoubleSquareArray(int m, bool ** arr){
+    arr = new bool*[m];
+    for(int i = 0; i < m; i++){
+        arr[i] = new bool[m];
+        for(int j = 0; j < m; j++){
+            arr[i][j] = false;
+        }
+    }
+}
+
+void freeDoubleSquareArray(int m, bool ** arr){
+    for(int i = 0; i < m; i++){
+        delete[] arr[i];
+    }
+    delete[] arr;
+}
+
+void readBoard(int m, int g, bool** board){
     string line;
     for(int i = g; i < m+g; i++){
         cin >> line; 
@@ -17,7 +35,7 @@ void readBoard(int m, int g, vector<vector<bool>>& board){
     }
 }
 
-void printBoard(int m, int g, vector<vector<bool>>& board){
+void printBoard(int m, int g, bool ** board){
     for(int i = g; i < m + g; i++){
         for(int j = g; j < m + g; j++){
             cout << (board[i][j] ? '#' : '.');
@@ -27,9 +45,7 @@ void printBoard(int m, int g, vector<vector<bool>>& board){
     cout << flush;
 }
 
-int getNeighCnt(int i, int j, vector<vector<bool>>& board) {
-    int m = board.size();
-
+int getNeighCnt(int i, int j, bool** board) {
     int cntNeigh = 0;
 
     cntNeigh += board[i][j-1];
@@ -46,11 +62,11 @@ int getNeighCnt(int i, int j, vector<vector<bool>>& board) {
     return cntNeigh;
 }
 
-void step(int g, vector<vector<bool>>& board){
+void step(int m, int g, bool ** board){
     vector<pair<int,int>> changed_cells;
 
-    for(int i = g; i < board.size()-g; i++){
-        for(int j = g; j < board.size()-g; j++){
+    for(int i = g; i < m + g; i++){
+        for(int j = g; j < m + g; j++){
             int neighCnt = getNeighCnt(i, j, board);
 
             if(board[i][j]){
@@ -73,12 +89,18 @@ int main(int argc, char* argv[]) {
     MPI_Comm_size(MPI_COMM_WORLD, &npes);
     MPI_Comm_rank(MPI_COMM_WORLD, &myrank);
 
+    int d = sqrt(npes);
+    if(d*d != npes) {
+        cerr << "Number of processes must be a perfect square" << endl;
+        return 1;
+    }
+
 
     int m; // size of board    
     int n; // final generation
     int g; // size of ghost cell
-    vector<vector<bool>> board; 
-    vector<vector<bool>> local_board; 
+    bool** board; 
+    bool** local_board; 
 
     if(myrank == 0){
         cin >> m >> n >> g; // read m, n, g from txt input
@@ -87,16 +109,36 @@ int main(int argc, char* argv[]) {
     MPI_Bcast(&n, 1, MPI_INT, 0, MPI_COMM_WORLD);
     MPI_Bcast(&g, 1, MPI_INT, 0, MPI_COMM_WORLD);
 
+    int board_size = m+2*g + (m+2*g) % d; //padding with dimmension
+    int l_board_size = board_size/d + 2*g; //local board size
+    int l_comp_size = board_size/d; //local computing size
+    
+    initDoubleSquareArray(l_board_size, local_board);
     if(myrank == 0){
-        board = vector<vector<bool>>(m+2*g, vector<bool>(m+2*g, false));
+        initDoubleSquareArray(board_size, board);
         readBoard(m, g, board); // read board from txt input
-    }; 
 
-    //TODO send the partition of the board to each process
-    //Partitioned board will be a 2D vector of size (m/root(npes) + 2g)
+        //TODO send the partition of the board to each process
+        //Partitioned board will be a 2D vector of size (d + 2g))
+        for(int ri = 0; ri < d; ri++){
+            for(int rj = 0; rj < d; rj++){
+                int rank = ri * d + rj;
+                //SEND (ri*l_comp_size, ri*l_comp_size + l_board_size) * (rj*l_comp_size, rj*l_comp_size + l_board_size)
+                for(int i = 0; i < l_board_size; i++){
+                    MPI_Send(&board[i + ri*l_comp_size][rj*l_comp_size], l_board_size, MPI_CXX_BOOL, rank, i, MPI_COMM_WORLD);
+                }
+            }
+        }
+    }; 
+    for(int i = 0; i < l_board_size; i++){
+        MPI_Send(&local_board[i][0], l_board_size, MPI_CXX_BOOL, myrank, i, MPI_COMM_WORLD);
+    }
+    cout << "myrank: "<< myrank << endl;
+    printBoard(l_board_size, g, local_board);
+
 
     for(int gen = 0; gen < n; gen++){
-        step(g, board);
+        // step(local_m, g, local_board);
         
         //TODO send the ghost cells value to each process
     }
@@ -104,7 +146,7 @@ int main(int argc, char* argv[]) {
     //TODO gather the board from each process to the root process
 
     if(myrank == 0){
-        printBoard(m, g, board);
+        // printBoard(m, g, board);
     }
 
     MPI_Finalize();
